88dd5ada5a4a9232f4440d2637aac666a4adf747
Ashwin <bsashwin9@gmail.com>
2021-12-03 07:59:00 +0530

    AoC 2021 Day 1

---
+++ 2021/Day 1 - Sonar Sweep/1.py
@@ -0,0 +1,16 @@
+from itertools import islice, tee
+
+
+def pairwise(iterable, offset=1):
+    a, b = tee(iterable)
+    return zip(a, islice(b, offset, None))
+
+
+def compare(numbers, n):
+    return sum(x < y for x, y in pairwise(numbers, offset=n))
+
+
+with open("input.txt") as fp:
+    numbers = [int(line.strip()) for line in fp.readlines()]
+
+    print(compare(numbers, 1))
