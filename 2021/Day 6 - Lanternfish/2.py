0725d2ee9c72c27cc2e09aa3d76ff39160e21aca
Ashwin <bsashwin9@gmail.com>
2021-12-06 12:55:12 +0530

    AoC 2021 Day 6

---
+++ 2021/Day 6 - Lanternfish/2.py
@@ -0,0 +1,18 @@
+with open("input.txt") as f:
+    values = [int(i) for i in f.readline().split(",")]
+
+
+fish = {i: 0 for i in range(-1, 9)}
+
+for val in values:
+    fish[val] += 1
+
+for i in range(256):
+    for j in range(len(fish) - 1):
+        fish[j - 1] = fish[j]
+
+    fish[8] = fish[-1]
+    fish[6] += fish[-1]
+    fish[-1] = 0
+
+print(sum(list(fish.values())))
